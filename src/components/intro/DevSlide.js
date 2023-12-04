8e7dc1f6821cb6070d1dfa44d81b3b4cc1ffc17f
rogi-mini <yrkim6883@gmail.com>
2023-12-05 04:08:29 +0900

    dev-slide 내용 추가 및 react markdown 설치

--- src/components/intro/DevSlide.js
+++ src/components/intro/DevSlide.js
@@ -1,14 +1,28 @@
+import ReactMarkdown from "react-markdown";
+
 const DevSlide = () => {
   return (
     <div className="dev-wrapper">
-      <a
-        href="https://github.com/rogi-rogi/KNU_Graduation_Guidance"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Github
-      </a>
+      <div className="dev-icon-wrapper">
+        <ReactMarkdown
+          components={{
+            img: ({ node, ...props }) => (
+              <img style={{ width: "700px" }} {...props} alt="dev-logo" />
+            ),
+          }}
+        >
+          {SkillIcons}
+        </ReactMarkdown>
+      </div>
+      <div className="div-bar"></div>
+      <div className="footer">2023-2 강남대학교 웹개발응용 기말프로젝트</div>
     </div>
   );
 };
+const SkillIcons = `
+
+[![My Skills](https://skillicons.dev/icons?i=js,react,scss,figma,vscode,git)](https://github.com/rogi-rogi/KNU_Graduation_Guidance)
+
+`;
+
 export default DevSlide;
